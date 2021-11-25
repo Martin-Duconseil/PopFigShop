c584035351a713ee95b82534273da05e9008481f
PaulROBBE <paul.robberechts@mmibordeaux.com>
2021-11-25 14:34:07 +0100

    fiche produit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import './App.css';
 function App() {
   return (
     <div>
+
       <h1>Bienvenue sur PopFigShop !</h1>
+
+      <div id="fiche produit">
+        <h2>Figurine Pop</h2>
+
+        <img src="https://placekitten.com/300/300"/> <br></br>
+
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the...</p>
+
+        <p>Prix = 15.99€</p>
+
+        <button>Ajouter au panier</button><br></br>
+      </div>
+
     </div>
+
+    
   );
 }
 
